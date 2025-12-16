16b171549aaf9c59736afd9c65430ab0d80dcbda
Thasleema <thasleemapk133@gmail.com>
2025-12-16 23:52:57 +0530

    the functioning done

--- js/index.js
+++ js/index.js
@@ -1,48 +1,52 @@
 let item = document.getElementById("item")
 let btn = document.getElementById("btn")
 let list = document.getElementById("list")
-btn.addEventListener("click", (event) => {
-   event.preventDefault()
-    if(item.value.trim()==="")
-         {
-            alert("Enter a Task")
-            return}
 
-         let li = document.createElement("li")
+btn.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (item.value.trim() === "") {
+        alert("Enter a Task")
+        return
+    }
+
+    let li = document.createElement("li")
     let p = document.createElement("span")
     p.textContent = item.value
     li.appendChild(p)
-        item.value = ""
-    const a=document.createElement("div")
-       const edit = document.createElement("button")
+    item.value = ""
+    list.appendChild(li)
+
+
+    const a = document.createElement("div")
+    const edit = document.createElement("button")
     edit.textContent = "EDIT"
     a.appendChild(edit)
 
+    edit.addEventListener("click", (e) => {
+        if (edit.textContent === "EDIT") {
+            p.contentEditable = "true"
+            p.focus()
+            edit.innerText = "Save"
+        } else {
+            p.contentEditable = "false"
+            edit.innerText = "Edit"
 
-     const del = document.createElement("button")
+        }
+    })
+
+    const del = document.createElement("button")
     del.textContent = "DELETE"
-    del.id="del"
-a.appendChild(del)
+    del.id = "del"
+    a.appendChild(del)
+    del.addEventListener("click", () => {
+        li.remove()
+    })
+    li.appendChild(a)
 
-li.appendChild(a)
+    p.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') { btn.click() }
+})
+})
 
-list.appendChild(li)
 
-})
-edit.addEventListener("click",(e)=>{
-if(edit.textContent==="EDIT"){
-    text.contentEditable="true"
-    text.focus()
-    edit.innerText="Save"
-}else{
-    text.contentEditable="false"
-        edit.innerText="Edit"
 
-}
-})
-del.addEventListener("click",()=>{
-    li.remove()
-})
-item.addEventListener('keypress',(e)=>{
-    if(e.key==='Enter'){btn(e)}
-})
